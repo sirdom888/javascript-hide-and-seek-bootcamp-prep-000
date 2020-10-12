a0afbf8ad217a4c7d717f217946ac0c349300de7
Dom Siravo <domcode88@gmail.com>
2020-10-12 19:09:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function getFirstSelector(selector) {
 
 function nestedTarget() {
   const nested = document.getElementById('nested');
-  const target = document.
+  const target = document.querySelector
 }
