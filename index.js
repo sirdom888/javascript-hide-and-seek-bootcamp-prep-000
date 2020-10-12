0d661b71b04e9bd5c87868f5401470c86b1799a4
Dom Siravo <domcode88@gmail.com>
2020-10-12 19:49:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function getFirstSelector(selector) {
+  const ele = document.querySelector(selector);
+  return ele;
+}
 
+function nestedTarget() {
+  const target = document.getElementById('nested').querySelector('div.target')
+  return target;
+}
+
+function increaseRankBy(n) {
+  const rank = document.querySelectorAll('ul.ranked-list li');
+  
+  for (var i = 0; i < rank.length; i++) {
+    rank[i].innerHTML = parseInt(rank[i].innerHTML) + n;
+  }
+  
+}
+
+function deepestChild() {
+  const grandNode = document.querySelector('#grand-node').querySelectorAll('div');
+  return grandNode[grandNode.length - 1];
+  
+}
